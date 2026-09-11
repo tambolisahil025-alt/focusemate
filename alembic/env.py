f92584be015600ed9ad3426e93dc0a0a76e60a56
Sahil Tamboli <tambolisahil025@gmail.com>
2026-09-11 16:15:46 +0530

    Update Expo dependencies and project changes

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,7 @@ from app.db.models import Base
 config = context.config
 
 # 3. Force Alembic to use the DATABASE_URL from your .env file!
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("%", "%%"))
 
 # Interpret the config file for Python logging.
 if config.config_file_name is not None:
